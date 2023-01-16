2fb2fe6c4e027ed22ac1f2b20f1a3368d00e9750
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-01-16 20:55:34 +0530

    open modal

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let btnCloseModal = document.querySelector('.close-modal');
+let btnShowModal = document.querySelectorAll('.show-modal');
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(i);
+  btnShowModal[i].addEventListener('click', openModal);
+}
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
